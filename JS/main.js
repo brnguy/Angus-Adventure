db3d6262be62045b6820a53dd622e98a309dcb04
brnguy <nguyen.bryan15@gmail.com>
2022-02-04 14:46:38 -0800

    added layout for fight stage and event listener to start button

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+/* DOM SELECTORS -- EVENT LISTENERS */
+
+/* GAME STATE/CANVAS RENDERING STUFF */
+
+/* OBJECT FACTORY */
+
+/* FUNCTIONS */
+
+// Start Button -- EXTRA (FADE OUT)
+document.querySelector('button').addEventListener('click', () => {
+    startScreen.style.display = "none"
+    fightStage.style.display = "grid"
+})
